b9f3e2a3345d1fed98a59cf4520b620a0ec5f0c2
Elyott Ryan <elyott_ryan@hotmail.com>
2018-03-18 17:58:08 +0000

    Made a simple request

--- https_example.js
+++ https_example.js
@@ -1,3 +1,19 @@
 var https = require('https');
 
 console.log('I did it!');
+
+var options = {
+  host: 'www.example.org',
+  path: '/'
+};
+
+// called by https when the request is made.
+var callback = function(){
+  console.log('In response handler callback');
+}
+
+console.log('I am about to make a request');
+
+https.request(options, callback).end();
+
+console.log('I made the request');
